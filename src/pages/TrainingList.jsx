1060ba8901b25030f1a45ddb9d1ce222dc87f358
Menard <perez.menard.nomiddlename@gmail.com>
2025-04-04 22:49:25 +0800

    Added: sending attachments

--- src/pages/TrainingList.jsx
+++ src/pages/TrainingList.jsx
@@ -1,22 +1,41 @@
+import axios from "axios";
 import React from "react";
 
 function TrainingList() {
-  const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
-  const handleSendEmail = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch("http://localhost/backend/api.php");
+  const [input, setInput] = React.useState(null);
+  const [uploadProgress, setUploadProgress] = React.useState(0);
 
-      const data = await response.json();
-      if (data.status === "error") {
-        setData(data);
-      }
-      setData(data);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setInput(file);
+    }
+  };
+  const handleSendFile = async () => {
+    const formData = new FormData();
+    formData.append("file", input);
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost/backend/api.php",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            const { loaded, total } = progressEvent;
+            const percentCompleted = Math.round((loaded * 100) / total);
+            setUploadProgress(percentCompleted);
+          },
+        }
+      );
       setLoading(false);
+      return response.data;
     } catch (error) {
-      console.error("Error fetching data:", error);
       setLoading(false);
+      console.error("Error uploading file:", error);
     } finally {
       setLoading(false);
     }
@@ -24,9 +43,10 @@ function TrainingList() {
   return (
     <div>
       <h1>Training List</h1>
-      <button onClick={handleSendEmail}>Send Training List</button>
+      <button onClick={handleSendFile}>Send Training List</button>
+      <input type='file' onChange={handleFileChange} required />
       {loading && <p>Loading...</p>}
-      {data && data.message}
+      {uploadProgress > 0 && <p>Upload Progress: {uploadProgress}%</p>}
     </div>
   );
 }
