fe1d5c8c67070a478b8e92b6623b632566f9dff1
Menard <perez.menard.nomiddlename@gmail.com>
2025-04-05 12:35:33 +0800

    temporary shit

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import Quill from "quill";
-import "quill/dist/quill.snow.css"; // Import the Quill CSS
+import "quill/dist/quill.snow.css";
+import QuillResizeImage from "quill-resize-image";
+Quill.register("modules/resize", QuillResizeImage);
+
 function Create() {
   const editorRef = React.useRef(null);
   const quillInstanceRef = React.useRef(null);
   React.useEffect(() => {
-    if (editorRef.current) {
-      quillInstanceRef.current = new Quill(editorRef.current, {
-        theme: "snow",
-        modules: {
-          toolbar: [
-            [{ header: [1, 2, false] }],
-            ["bold", "italic", "underline"],
-            ["image", "code-block"],
-            ["clean"],
-          ],
+    if (!editorRef.current) return;
+    if (quillInstanceRef.current) return;
+    quillInstanceRef.current = new Quill(editorRef.current, {
+      theme: "snow",
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline"],
+          ["image", "code-block"],
+          ["clean"],
+        ],
+        resize: {
+          modules: ["Resize", "DisplaySize", "Toolbar"], // Enable specific sub-modules
+          handleStyles: {
+            backgroundColor: "blue", // Customize handle styles
+            border: "1px solid white",
+          },
+          minWidth: 50, // Set minimum width for resizing
+          minHeight: 50, // Set minimum height for resizing
+          maxWidth: 800, // Set maximum width for resizing
+          maxHeight: 800, // Set maximum height for resizing
         },
-      });
-    }
+      },
+    });
   }, []);
   return <div ref={editorRef} className='quill-editor'></div>;
 }
