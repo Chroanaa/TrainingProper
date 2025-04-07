48b8ff8260564b277d8e55c0ec0e44c2b2626d2b
tempestDev365 <alfonso.christian.macatangay@gmail.com>
2025-04-08 01:40:45 +0800

    added navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router";
+import Navigation from "./Navigation";
+
 function Header() {
   return (
-    <div>
-      <NavLink to='/SendDocument' className='text-2xl font-bold text-blue-500'>
-        Send Syllabus
-      </NavLink>
-      <NavLink to='/Create' className='text-2xl font-bold text-blue-500'>
-        Create Training
-      </NavLink>
-      <NavLink to='/List' className='text-2xl font-bold text-blue-500'>
-        Training List
-      </NavLink>
+    <div className="flex">
+      <Navigation />
+      <div className="border-2 border-gray-300 bg-white w-full h-screen p-5">
+           <NavLink to='/SendDocument' className='text-2xl font-bold text-red-500'>
+                    Send Syllabus
+             </NavLink>
+             <NavLink to='/Create' className='text-2xl font-bold text-blue-500'>
+                  Create Training
+              </NavLink>
+             <NavLink to='/List' className='text-2xl font-bold text-blue-500'>
+                  Training List
+              </NavLink>
+      </div>
     </div>
   );
 }
