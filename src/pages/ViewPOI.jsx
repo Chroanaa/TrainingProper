f49162d628ce4a9cb68645821b556f0422b11db5
tempestDev365 <alfonso.christian.macatangay@gmail.com>
2025-05-13 22:15:24 +0800

    change some ui in viewpo

--- src/pages/ViewPOI.jsx
+++ src/pages/ViewPOI.jsx
@@ -60,13 +60,17 @@ function ViewPOI() {
   };
   return (
     <div>
-      <input type='text' value={newTitle} onChange={handleOnChange} />
+      <div className='flex mb-2 gap-2'>
+      <input  className="px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent shadow-sm transition-all duration-200 "  type='text' value={newTitle} onChange={handleOnChange} />
+      <div className='flex gap-1'>
       <Button variant='contained' onClick={handleOnSave}>
         Save
       </Button>
       <Button variant='contained' onClick={handleDownload}>
         Download
       </Button>
+      </div>
+      </div>
       <QuillComponent ref={quillRef} />
     </div>
   );
